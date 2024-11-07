cdcd5cf241bf7a372ceba9c2daa954e95a73ecfd
Leon <mutembedzaleon@gmail.com>
2024-11-07 11:14:34 +0100

    07/11

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -6,8 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
-#load dataset
-iris = pd.read_csv('Iris.csv')
+# Step 1: Collect Data from Arduino
 
 #features of x and y components
 X = iris.iloc[:, :-1]
@@ -24,6 +23,17 @@ y_pred = knn.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
 
-print("Predicted labels: ", y_pred)
-print("True labels: ", y_test)
-print(f"Accuracy: {accuracy * 100:.2f}%", )
+def plot_data_with_peaks(data, peaks):
+    """
+     Plots the pulse oximeter data and highlights the detected peaks.
+    :param data: NumPy array of pulse oximeter data
+    :param peaks: List of indices where peaks were detected
+     """
+    plt.plot(data, label='Pulse Oximeter Data')
+    plt.plot(peaks, data[peaks], "x", label='Detected Peaks', color='red')
+    plt.title("Pulse Oximeter Data from Arduino with Detected Peaks")
+    plt.xlabel("Time (Sample Index)")
+    plt.ylabel("Sensor Value")
+    plt.legend()
+    plt.show()
+# Step 4: Estimate Heart Rate
